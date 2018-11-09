05a59af3a182188fb618831e1ce5da0834b1cc38
hong <hong@ok.com>
2018-11-09 14:27:19 +0800

    采用spring IOC也称为依赖注入, 重构service与dao层,重构的步骤,参考spring-bean.xml的注释

--- src/cn/web/dao/ProductDaoImpl.java
+++ src/cn/web/dao/ProductDaoImpl.java
@@ -2,32 +2,25 @@ package cn.web.dao;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import cn.web.model.Product;
 
 // 完成Product的CRUD操作(增、删、改、查)
-public class ProductDaoImpl extends BaseDao<Product> {
+public class ProductDaoImpl {
 
-	public static void main(String[] args) {
-		ProductDaoImpl daoImpl = new ProductDaoImpl();
-		// Product product = new Product();
-		// product.setName("华为手机");
-		// product.setPrice(5999.99);
-		// product.setRemark("测试一下!!");
-		// product.setId(5);
-		// daoImpl.update(product);
-		// daoImpl.delete(5);
-		Product product = daoImpl.getById(1);
-		System.out.println(product);
-		List<Product> proList = daoImpl.queryByName("西服");
-		for (Product temp : proList) {
-			System.out.println(temp);
-		}
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		System.out.println("setJdbcTemplate...........");
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	public Product getById(int id) {
 		String sql = "select * from product where id = ?";
-		List<Product> proList = super.queryByName(Product.class, sql, id);
-		return proList.size() > 0 ? proList.get(0) : null;
+		return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<Product>(Product.class), id);
+
 	}
 
 	// ctrl + D 删除当前行
@@ -36,22 +29,22 @@ public class ProductDaoImpl extends BaseDao<Product> {
 	// shift + alt + A 开启列删除
 	public List<Product> queryByName(String keyword) {
 		String sql = "select * from product where name like ?";
-		return super.queryByName(Product.class, sql, "%" + keyword + "%");
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Product>(Product.class), "%" + keyword + "%");
 	}
 
 	public int update(Product product) {
 		String sql = "update product set name = ? ,price = ? ,remark = ? where id = ?";
-		return super.update(sql,
+		return jdbcTemplate.update(sql,
 				new Object[] { product.getName(), product.getPrice(), product.getRemark(), product.getId() });
 	}
 
 	public int delete(int id) {
 		String sql = "delete from product where id = ?";
-		return super.update(sql, id);
+		return jdbcTemplate.update(sql, id);
 	}
 
 	public int save(Product product) {
 		String sql = "insert into product (name,price,remark) values (?,?,?)";
-		return super.update(sql, new Object[] { product.getName(), product.getPrice(), product.getRemark() });
+		return jdbcTemplate.update(sql, new Object[] { product.getName(), product.getPrice(), product.getRemark() });
 	}
 }
--- src/cn/web/service/ProductServiceImpl.java
+++ src/cn/web/service/ProductServiceImpl.java
@@ -2,6 +2,9 @@ package cn.web.service;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import cn.web.dao.ProductDaoImpl;
 import cn.web.model.Product;
 
@@ -10,7 +13,24 @@ import cn.web.model.Product;
 // 显示层一般又会采用 MVC设计模式   V视图(JSP)   C控制层(Servlet)   M模型(Product)
 public class ProductServiceImpl {
 
-	private ProductDaoImpl productDao = new ProductDaoImpl();
+	private ProductDaoImpl productDao = null;
+
+	public static void main(String[] args) {
+		// ProductServiceImpl serviceImpl = new ProductServiceImpl();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-bean.xml");
+
+		// 所有的bean交给spring管理,因此在创建的时候,也需要采用Spring方式来创建bean
+		ProductServiceImpl serviceImpl = context.getBean("ps", ProductServiceImpl.class);
+		List<Product> proList = serviceImpl.queryByName("");
+		for (Product temp : proList) {
+			System.out.println(temp);
+		}
+	}
+
+	public void setProductDao(ProductDaoImpl productDao) {
+		System.out.println("setProductDao()..........");
+		this.productDao = productDao;
+	}
 
 	public Product getById(int id) {
 		return productDao.getById(id);
